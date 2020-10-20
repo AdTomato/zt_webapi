0c05d795b56cbdbd8d1fca31c10c4e4e2dced09a
xiaodouzi <1950746552@qq.com>
2020-10-20 13:09:35 +0800

    1. 删除多余jar包依赖

--- src/main/java/com/authine/cloudpivot/web/api/controller/EmployeeEvaluationController.java
+++ src/main/java/com/authine/cloudpivot/web/api/controller/EmployeeEvaluationController.java
@@ -21,7 +21,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
-import org.omg.PortableServer.LifespanPolicyValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
