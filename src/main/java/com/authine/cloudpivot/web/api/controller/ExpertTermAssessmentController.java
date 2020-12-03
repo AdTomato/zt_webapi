76f6d4e62bab485e8a5101bf3608cd1763b1462b
lfh <lfh>
2020-12-03 21:37:51 +0800

    评委人数判断

--- src/main/java/com/authine/cloudpivot/web/api/controller/ExpertTermAssessmentController.java
+++ src/main/java/com/authine/cloudpivot/web/api/controller/ExpertTermAssessmentController.java
@@ -139,9 +139,9 @@ public class ExpertTermAssessmentController extends BaseController {
 		expertTermAssessmentService.clearAssessmentScore(expertTermGradeInfos.getId());
 		// 判断流程状态 expert_assessment_detail
 		List<String> peoples = expertTermAssessmentService.getAssessMentDetailNum(expertTermGradeInfos.getId());
-		//if (peoples.size() != expertTermGradeInfos.getNum()){
-		//	return getErrResponseResult("error", 404L, "流程尚未结束，无需计算");
-		//}
+		if (peoples.size() != expertTermGradeInfos.getNum()){
+			return getErrResponseResult("error", 404L, "流程尚未结束，无需计算");
+		}
 		// 计算平均分
 		log.info("开始执行计算平均分的方法");
 		List<ExpertTermAvgScore> expertTermAvgScores = expertTermAssessmentService.countFinanceAvg(expertTermGradeInfos.getId());
